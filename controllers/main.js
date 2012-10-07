ccf2ef726d4d5ba7d0d0da253bb3b7cb9e2b2e06
tank <tank.tyt@gmail.com>
2012-10-07 10:57:43 +0800

    修改域名使用appfog

--- controllers/main.js
+++ controllers/main.js
@@ -4,5 +4,5 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Pretty',url:'192.168.1.151:3000' });
+  res.render('index', { title: 'Pretty',url:'pretty.ap01.aws.af.cm' });
 };
